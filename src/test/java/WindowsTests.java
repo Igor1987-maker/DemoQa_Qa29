c6e079069492a7085f0aa319a7109fd22bc62f2b
Igor <igorprozorow2000@gmail.com>
2022-03-02 14:49:35 +0200

    comit

--- src/test/java/WindowsTests.java
+++ src/test/java/WindowsTests.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -30,6 +31,9 @@ public class WindowsTests extends TestBase{
         app.windows().clickOnNewWindowMessage();
 
     }
-  //  @Aftermethod
+   @AfterMethod
+    public void dfsdf(){
+
+   }
 
 }
